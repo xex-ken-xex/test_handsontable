647a2746be405981f83a42f5a76f05ca01770f21
xex-ken-xex <kenichi.izawa@gmail.com>
2018-01-08 21:54:05 +0900

    検索に対応

--- app.js
+++ app.js
@@ -24,39 +24,44 @@ var table = new Handsontable(grid, {
 
 // https://www.tam-tam.co.jp/tipsnote/javascript/post11736.html
 var file = document.getElementById('file');
-var result = document.getElementById('result');
+var masterdataCount = document.getElementById('masterdataCount');
+
+csv_data = [];
 if(window.File && window.FileReader && window.FileList && window.Blob) {
     function loadLocalCsv(e) {
-        // ファイル情報を取得
+
         var fileData = e.target.files[0];
         console.log(fileData); // 取得した内容の確認用
    
+        file.value = null;
+        var cols = null;
+
         // CSVファイル以外は処理を止める
-        // todo: Excelが入っているとここが変な挙動になる？ → 要調査
         if(!fileData.name.match('.csv$')) {
             alert('CSVファイルを選択してください');
-            file.value = null;
             return;
         }
     
         // FileReaderオブジェクトを使ってファイル読み込み
         var reader = new FileReader();
-        var cols = null;
         
         // ファイル読み込みに成功したときの処理
         reader.onload = function() {
             var rows = reader.result.split('\n');
             console.info( rows.length, "件...読み出しました." );
-            data.length = 0;
+            csv_data.length = data.length = 0;
             for (var i = 1; i < rows.length; i++) {
                 row = rows[i].split(',');
                 var obj = { postcode: row[4],
                             prefname: row[7],
                             address:  (row[7]+row[9]+row[11]) };
                 data.push( obj );
+                masterdataCount.textContent = i.toLocaleString();
             }
-            rows = null;
+            csv_data = data.slice();
             table.render(); // 内部データをGUIへ反映
+            // 検索用メモリの作成
+            rows = null;
         }
         // ファイル読み込みを実行
         reader.readAsText(fileData);
@@ -68,17 +73,31 @@ if(window.File && window.FileReader && window.FileList && window.Blob) {
     result.innerHTML = 'このブラウザには対応していません.';
 }
 
-var searchbtn = document.getElementById('search');
-searchbtn.addEventListener('click', searchPostCode );
+// ボタンを押下した場合は、入力した場合に検索する.
+// TODO: 画面上で編集していたら反映しないとダメなんだけど、まだ反映していないよ。
+$('#searchPostCode').on('click', searchPostCode);
+$('#searchword').on('input', searchPostCode);
 function searchPostCode() 
 {
-    // 【処理内容】
-    // 検索結果行にあるデータを削除、一番上の行へ検索用マーカーを移動させる
-    // 検索対象の行を見つける
-    // 検索条件列に入っていた（3つのパラメータを確認して）ら、検索を実行
-    // 検索結果が1より大きかった場合は、行を足す
-    //     table.alter('insert_row', 0);
-    //     検索対象の行のため、追加した数ぶん下に検索用マーカーを移動
-    // 検索結果を右側に転記する
+    if( 0 == csv_data.length ) return ;
+    var result = null;
+
+    var reg = new RegExp("[^0-9\-]");   // 数字とハイフンじゃない 条件
+    var searchText = $(this).val();     // 検索ボックスに入力された値
+
+    if (searchText.match( reg ) ){
+        result = csv_data.filter( function( value ) {
+            return (value.address.indexOf( searchText ) >= 0 ? true : false)
+        })
+    } else {
+        result = csv_data.filter( function( value ) {
+            return (value.postcode.indexOf( searchText ) == 0 ? true : false)
+        })
+    }
+    data.length = 0;
+    for( i = 0 ; i < result.length ; i++ ){ // データをdataにコピーしたいんだけど.参照渡しじゃなくて値渡しでコピーする方法はないのかな？
+        data.push( result[i]);
+    }
+    table.render(); // 内部データをGUIへ反映
 }
 
